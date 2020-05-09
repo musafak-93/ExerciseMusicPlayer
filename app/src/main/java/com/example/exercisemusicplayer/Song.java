e36e9262a78298e0f098aee280b635ad27c09e05
Musafak <musafak.ft18@mail.umy.ac.id>
2020-05-09 17:06:38 +0700

    add konstruktor untuk memanggil variable music

--- app/src/main/java/com/example/exercisemusicplayer/Song.java
+++ app/src/main/java/com/example/exercisemusicplayer/Song.java
@@ -5,4 +5,10 @@ public class Song {
     private long id;
     private String title;
     private String artist;
+
+    public Song(long songID, String songTitle, String songArtist) {
+        id=songID;
+        title=songTitle;
+        artist=songArtist;
+    }
 }
