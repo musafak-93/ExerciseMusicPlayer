2306c6cfe4f997b1d5a527bc89b89dde7d6026be
Musafak <musafak.ft18@mail.umy.ac.id>
2020-05-10 23:07:06 +0700

    add variable objek

--- app/src/main/java/com/example/exercisemusicplayer/MusicService.java
+++ app/src/main/java/com/example/exercisemusicplayer/MusicService.java
@@ -2,10 +2,24 @@ package com.example.exercisemusicplayer;
 
 import android.app.Service;
 import android.media.MediaPlayer;
+import java.util.ArrayList;
+import android.content.ContentUris;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Binder;
+import android.os.PowerManager;
+import android.util.Log;
 
 public class MusicService extends Service implements
         MediaPlayer.OnPreparedListener,
         MediaPlayer.OnErrorListener,
         MediaPlayer.OnCompletionListener{
 
+    //media player
+    private MediaPlayer player;
+    //song list
+    private ArrayList<Song> songs;
+    //current position
+    private int songPosn;
 }
