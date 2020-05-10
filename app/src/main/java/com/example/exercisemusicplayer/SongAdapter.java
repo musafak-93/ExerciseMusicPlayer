7c30eb2013ccaaf635eda5bd673b8da4904b1be9
Musafak <musafak.ft18@mail.umy.ac.id>
2020-05-10 22:49:27 +0700

    add SongAdapter untuk memetakan lagu pada tampilan daftar

--- app/src/main/java/com/example/exercisemusicplayer/SongAdapter.java
+++ app/src/main/java/com/example/exercisemusicplayer/SongAdapter.java
@@ -1,4 +1,36 @@
 package com.example.exercisemusicplayer;
 
-public class SongAdapter {
+import java.util.ArrayList;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class SongAdapter extends BaseAdapter {
+
+    //song list and layout
+    private ArrayList<Song> songs;
+    private LayoutInflater songInf;
+
+    //constructor
+    public SongAdapter(Context c, ArrayList<Song> theSongs) {
+        songs = theSongs;
+        songInf = LayoutInflater.from(c);
+    }
+
+    @Override
+    public int getCount() {
+        return songs.size();
+    }
+
+    @Override
+    public Object getItem(int arg0) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int arg0) {
+        return 0;
+    }
 }
