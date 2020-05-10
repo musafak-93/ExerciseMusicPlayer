04d6ebc320d7c9aad0e0bcfdf650db3eb5b8116e
Musafak <musafak.ft18@mail.umy.ac.id>
2020-05-10 23:25:23 +0700

    add class ServerConnection untuk koneksi ke Music service

--- app/src/main/java/com/example/exercisemusicplayer/MainActivity.java
+++ app/src/main/java/com/example/exercisemusicplayer/MainActivity.java
@@ -83,4 +83,23 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    //connect to the service
+    private ServiceConnection musicConnection = new ServiceConnection(){
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            MusicService.MusicBinder binder = (MusicService.MusicBinder)service;
+            //get service
+            musicService = binder.getService();
+            //pass list
+            musicService.setList(songList);
+            musicBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            musicBound = false;
+        }
+    };
+
 }
