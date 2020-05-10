871030998942ec867a40a9b15f25d799db976ec1
Musafak <musafak.ft18@mail.umy.ac.id>
2020-05-10 23:36:37 +0700

    add class play song untuk play music

--- app/src/main/java/com/example/exercisemusicplayer/MusicService.java
+++ app/src/main/java/com/example/exercisemusicplayer/MusicService.java
@@ -68,4 +68,23 @@ public class MusicService extends Service implements
         player.release();
         return false;
     }
+
+    public void playSong(){
+        player.reset();
+        //get song
+        Song playSong = songs.get(songPos);
+        //get id
+        long currSong = playSong.getID();
+        //set uri
+        Uri trackUri = ContentUris.withAppendedId(
+                android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                currSong);
+        try{
+            player.setDataSource(getApplicationContext(), trackUri);
+        }
+        catch(Exception e){
+            Log.e("MUSIC SERVICE", "Error setting data source", e);
+        }
+        player.prepareAsync();
+    }
 }
