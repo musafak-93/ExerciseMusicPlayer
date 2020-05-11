4bfead7910ba7dce91aeb8f7143f1e370fa37ee9
Musafak <musafak.ft18@mail.umy.ac.id>
2020-05-11 16:08:46 +0700

    add metode MediaController untuk tombol play/pause, rewind, fast-forward dan skip (previous/next)

--- app/src/main/java/com/example/exercisemusicplayer/MusicController.java
+++ app/src/main/java/com/example/exercisemusicplayer/MusicController.java
@@ -1,4 +1,13 @@
 package com.example.exercisemusicplayer;
 
-public class MusicController {
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.MediaController;
+
+public class MusicController extends MediaController {
+    public MusicController(Context context) {
+        super(context);
+    }
+
+    public void hide(){}
 }
